99699c3d0e2dafb18878836ea5fefb8545e13f1d
gg90052 <scmou104@gmail.com>
2015-08-18 12:29:11 +0800

    a

--- comment_helper/js/controller.js
+++ comment_helper/js/controller.js
@@ -152,7 +152,7 @@ myApp.controller('Tbody', function($scope,$filter){
 				clearInterval(t);
 				$.post("http://teddy.acsite.org/comment_helper_test/index2.php/main/checkvip",{"fbid":$scope.userid},function(res){
 					if (res == "false"){
-						$scope.comments = new Array();
+						$scope.update();
 						bootbox.alert("社團文章需要付費才能抓喔!!");
 					}
 				});

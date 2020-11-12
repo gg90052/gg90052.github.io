5c104abf83e2aa8959b4e4b20d51e15e663fea92
Teddy Huang <teddy.huang@icsgame.com>
2020-11-12 10:27:54 +0800

    test

--- comment_helper_plus/es6js/main_test.js
+++ comment_helper_plus/es6js/main_test.js
@@ -199,7 +199,7 @@ let config = {
 	target: '',
 	command: '',
 	order: 'chronological',
-	auth: 'groups_show_list, pages_show_list, pages_read_engagement, pages_read_user_content',
+	auth: 'groups_show_list, pages_show_list, pages_read_engagement, pages_read_user_content, groups_access_member_info',
 	likes: false,
 	pageToken: false,
 	userToken: '',

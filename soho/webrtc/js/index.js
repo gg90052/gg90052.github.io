5fff1d6d8f1c8c6f5a202ed1aac55d55cb4016a4
gg90052 <scmou104@gmail.com>
2022-07-27 11:25:40 +0800

    no message

--- soho/webrtc/js/index.js
+++ soho/webrtc/js/index.js
@@ -55,7 +55,7 @@ window.onbeforeunload = function () {
 }
 
 function connect(token, callback) {
-	ws = new WebSocket('wss://172.105.193.65:8888');
+	ws = new WebSocket('wss://172.105.193.65');
 	ws.onopen = () => {
 		sendMessage({
 			type: 'token',

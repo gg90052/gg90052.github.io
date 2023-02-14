087ea02f6af4f3bea7b6a5306624626efa5fe907
gg90052 <scmou104@gmail.com>
2023-02-14 09:14:34 +0800

    privacy

--- comment_helper_vue/src/components/mainArea/Ci.vue
+++ comment_helper_vue/src/components/mainArea/Ci.vue
@@ -3,10 +3,12 @@
     <div class="ci w-[250px] h-[225px] mb-4 mx-auto">
       <img class="w-full h-full" src="../../assets/ci.png" alt="">
     </div>
+
     <!-- <div class="w-full text-center">
       <input type="text" class="border pl-2 w-1/2 h-8 align-middle" placeholder="1208"/>
       <button class="btn-free-height btn-blue h-8 align-middle">驗證帳號</button>
     </div> -->
+    <a class="text-blue-500" href="https://www.privacypolicies.com/live/fc04b8f4-49ef-4fab-b7c3-505d38577df9" target="_blank">Privacy Policy</a>
   </div>
 </template>
 

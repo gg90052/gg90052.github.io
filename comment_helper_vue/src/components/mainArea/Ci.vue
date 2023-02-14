3da5031e0a458024cbf45a38ac2e3ffe58490283
gg90052 <scmou104@gmail.com>
2023-02-14 15:53:03 +0800

    policy

--- comment_helper_vue/src/components/mainArea/Ci.vue
+++ comment_helper_vue/src/components/mainArea/Ci.vue
@@ -8,7 +8,7 @@
       <input type="text" class="border pl-2 w-1/2 h-8 align-middle" placeholder="1208"/>
       <button class="btn-free-height btn-blue h-8 align-middle">驗證帳號</button>
     </div> -->
-    <a class="text-blue-500" href="https://www.privacypolicies.com/live/fc04b8f4-49ef-4fab-b7c3-505d38577df9" target="_blank">Privacy Policy</a>
+    <a class="text-blue-500" href="https://gg90052.github.io/comment_helper/policy.html" target="_blank">Privacy Policy</a>
   </div>
 </template>
 

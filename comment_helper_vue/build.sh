6eb3e7580a62a5664d2dcd4d152a01c8df0f7a1d
gg90052 <scmou104@gmail.com>
2023-06-06 09:53:39 +0800

    remove page

--- comment_helper_vue/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-npm i
-npm install babel-plugin-import --save-dev
-npm run build
-cp -Rvp $1/dist/. /var/www/html
--- comment_helper_vue/src/components/resultArea/PayCheck.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="bg-white border p-4 mb-2 flex justify-center flex-wrap">
-    <p class="mb-2 w-full">抓分享、社團抓留言為付費功能，詳情請看：<a class="text-blue-500" href="https://gg90052.github.io/comment_helper/pay.html" target="_blank">付費說明</a></p>
-    <SnInput></SnInput>
-  </div>
-</template>
-<script lang="ts" setup>
-import SnInput from '@/components/SnInput.vue';
-</script>

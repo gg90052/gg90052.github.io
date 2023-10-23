5043b82bc3d284ad4f83fd1a08b52369f0f47984
Teddy <>
2023-10-23 22:15:52 +0800

    ad block

---
+++ goom/assets/useLocalStorageState-0f18d97b.js
@@ -0,0 +1,1 @@
+import{r}from"./index-718a40ab.js";function l(o,t){const[e,s]=r.useState(()=>{const a=localStorage.getItem(t);return a?JSON.parse(a):o});return r.useEffect(()=>{localStorage.setItem(t,JSON.stringify(e))},[e,t]),[e,s]}export{l as u};

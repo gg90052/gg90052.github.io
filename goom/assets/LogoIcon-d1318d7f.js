6aac789a022c31bcb385e10df53d9f92d00c256d
Teddy <>
2023-10-24 19:54:43 +0800

    pwa test

--- goom/assets/LogoIcon-d1318d7f.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-1657ebe9.js";function n(){return o.jsx("div",{className:"w-8",children:o.jsx("img",{className:"w-full",src:"logoicon.png",alt:""})})}export{n as L};
---
+++ goom/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/goom/sw.js', { scope: '/goom/' })})}

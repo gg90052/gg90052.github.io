2ba91ec0adcdfc13c5c0e198522e93e62405cd76
Teddy <>
2023-12-04 15:21:30 +0800

    icon change

--- goom/assets/useLocalStorageState-d17856bb.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./index-2fdded0f.js";function l(o,t){const[e,s]=r.useState(()=>{const a=localStorage.getItem(t);return a?JSON.parse(a):o});return r.useEffect(()=>{localStorage.setItem(t,JSON.stringify(e))},[e,t]),[e,s]}export{l as u};

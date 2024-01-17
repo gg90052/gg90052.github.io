54ddffa720f533864bd54edd5e449fd43fb14529
Teddy <>
2024-01-17 09:48:15 +0800

    privacy

---
+++ goom/assets/Icon-889d1a3c.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-65759b21.js";import{s as c}from"./utils-1a6370a4.js";function l({iconId:s,src:o,size:e=6}){const t=JSON.parse(localStorage.getItem("icons"))||{},r=s?t[c(s)]:o||"defaultMarker.png";return a.jsx("div",{className:`w-${e} aspect-auto overflow-hidden w`,children:a.jsx("img",{className:"w-full",src:r,alt:""})})}export{l as I};
